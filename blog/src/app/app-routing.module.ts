7f8a2efc8cbe3f50691001498a37593c2fbd0fc1
= <rodolfolippo0405@gmail.com>
2024-04-15 17:34:58 +0200

    Menu Nav en cabecera

--- blog/src/app/app-routing.module.ts
+++ blog/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { InicioComponent } from './inicio/inicio.component';
+import { BlogComponent } from './blog/blog.component';
+import { SobremiComponent } from './sobremi/sobremi.component';
+import { ContactoComponent } from './contacto/contacto.component';
+const routes: Routes = [
+  {path:'inicio',component:InicioComponent},
+  {path:'blog',component:BlogComponent},
+  {path:'sobremi',component:SobremiComponent},
+  {path:'contacto',component:ContactoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
